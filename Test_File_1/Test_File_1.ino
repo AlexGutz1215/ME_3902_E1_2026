d2c5f7a5f80c0872d17f200e74a285507d933421
LadderMcLadder <adgutierrez0311@gmail.com>
2026-06-24 17:10:23 -0400

    Updated LED code with 2 Push Buttons
    Updated LED code with 2 Push Buttons

--- Test_File_1/Test_File_1.ino
+++ Test_File_1/Test_File_1.ino
@@ -1,15 +1,19 @@
 const int LED1_Pin=2;
-
 const int button1_Pin=3;
 
+const int LED2_Pin=5;
+const int button2_Pin=6;
+
 int button1_state=0;
+int button2_state=0;
 
 void setup() {
 	// Initialize serial communication - allows printing to the console for debugging.
 	Serial.begin(9600);
 	pinMode(LED1_Pin, OUTPUT); // Set the LED anode pin as an output
-
+  pinMode(LED2_Pin, OUTPUT);
   pinMode(button1_Pin, INPUT);
+  pinMode(button2_Pin, INPUT);
 }
 
 
@@ -24,14 +28,28 @@ void loop() {
 
   if (button1_state==HIGH)
   {
-    Serial.println("Button is Pressed"); // Print a message to output terminal
+    Serial.println("Button 1 is Pressed"); // Print a message to output terminal
     digitalWrite(LED1_Pin, HIGH); // Turn LED on
   }
 
   else
   {
-    Serial.println("Button is no longer Pressed"); // Print a message to output terminal
+    Serial.println("Button 1 is no longer Pressed"); // Print a message to output terminal
     digitalWrite(LED1_Pin, LOW); // Turn LED off
   }
-  delay(100);
+
+  button2_state = digitalRead(button2_Pin);
+
+  if (button2_state==HIGH)
+  {
+    Serial.println("Button 2 is Pressed"); // Print a message to output terminal
+    digitalWrite(LED2_Pin, HIGH); // Turn LED on
+  }
+
+  else
+  {
+    Serial.println("Button 2 is no longer Pressed"); // Print a message to output terminal
+    digitalWrite(LED2_Pin, LOW); // Turn LED off
+  }
+  delay(500);
 }
