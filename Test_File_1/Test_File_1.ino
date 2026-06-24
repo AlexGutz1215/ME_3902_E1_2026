7b04ddb97a92ddba9fcf1d1875398a8ba4cfd547
LadderMcLadder <adgutierrez0311@gmail.com>
2026-06-24 15:40:33 -0400

    Update Test_File_1.ino
    Testing with one LED

--- Test_File_1/Test_File_1.ino
+++ Test_File_1/Test_File_1.ino
@@ -1,10 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
-
+	// Initialize serial communication - allows printing to the console for debugging.
+	Serial.begin(9600);
+	pinMode(2, OUTPUT); // Set the LED anode pin as an output
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(2, HIGH); // Turn the LED on
+  delay(1000); // Wait for 1 second
+  digitalWrite(2, LOW); // Turn the LED off
+  delay(1000); // Wait for 1 second before repeating
 }
-jjjjjjjjj
