e0cc4090ce6de78e837cc7f4ceb01ea230abab40
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-02 18:29:48 -0400

    Updated ESP32 Test 6 - Both Sensors
    Code has been added for both sensor to print statements to serial monitor

--- Testing_ESP32_Test_6_A/Testing_ESP32_Test_6_A.ino
+++ Testing_ESP32_Test_6_A/Testing_ESP32_Test_6_A.ino
@@ -1,9 +1,18 @@
 #include "DHT.h"
+#include "math.h"
 
 #define DHTPIN 23
 #define DHTTYPE DHT22
 
+#define THERMISTORPIN 34
+
 DHT dht(DHTPIN, DHTTYPE);
+
+const float SeriesResistor = 10000; // 10 k resistor
+const float NominalResistance = 10000; // resistance at 25 degrees
+const float NominalTemperature = 25.0;
+const float BCoefficient = 3950.0; // Spec sheet
+const float ADCMax = 4095.0; // ESP32 ADC resolution
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,5 +39,27 @@ void loop() {
   Serial.println(humidity);
   Serial.print("Temperature in degree C: ");
   Serial.println(temperature);
+
+  float AdcVal = analogRead(THERMISTORPIN);
+
+  if (AdcVal >= ADCMax) AdcVal - ADCMax - 1;
+  if (AdcVal <= 0) AdcVal = 1;
+  // To avoid issues with /0
+
+  float resistance = SeriesResistor / ((ADCMax/AdcVal) - 1.0);
+
+  float steinhart;
+  steinhart = resistance / NominalResistance;
+  steinhart = log(steinhart);
+  steinhart /= BCoefficient;
+  steinhart += 1.0/(NominalTemperature + 273.15);
+  steinhart = 1.0/steinhart;
+
+  float tempC = steinhart - 273.15;
+
+  Serial.print("Thermistor Temp in deg C: ");
+  Serial.println(tempC);
+
+  delay(1000);
 
 }
