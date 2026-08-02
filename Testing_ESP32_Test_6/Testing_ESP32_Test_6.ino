8c0c3ce6ba536f5c7b2a86b1140e35ce7fa6ebec
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-02 14:40:23 -0400

    Created Testing ESP32 Code
    Created first version of the file

---
+++ Testing_ESP32_Test_6/Testing_ESP32_Test_6.ino
@@ -0,0 +1,18 @@
+// Motor Driver Pin Definitions
+
+const int enaA = 14;
+const int in1 = 27;
+const int in2 = 26;
+
+void setup()
+{
+  //Serial Monitor
+  Serial.begin(115200):
+
+  delay(50)
+}
+
+void loop()
+{
+  
+}
