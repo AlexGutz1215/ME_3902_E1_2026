9a2d12ce80d1ed30b4b910d8d69295ac83928c63
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-03 11:07:25 -0400

    Updated Test 8 Wheatstone code
    Updated the code for the wheatstone circuit

--- Test_8_Wheatstone_Bridge_Code.py
+++ Test_8_Wheatstone_Bridge_Code.py
@@ -6,13 +6,14 @@ adc_B = machine.ADC(27)
 
 VOLTAGE_FACTOR = 3.3 / 65535
 
-print("--- Wheatstone Bridge Live Test ---")
+print("--- Wheatstone Bridge Live Test (1k / 2k Pairs) ---")
+print("Expected Balance Point: ~2.2000 V")
 print("Press Ctrl+C to stop.\n")
 print(f"{'V_A (V)':<10}{'V_B (V)':10}{'V_out (Diff)':<15}")
  
 while True:
-    raw_A = sum([adc_A.read_u16() for _ in range(20)]) / 20
-    raw_B = sum([adc_B.read_u16() for _ in range(20)]) / 20
+    raw_A = sum([adc_A.read_u16() for _ in range(30)]) / 30
+    raw_B = sum([adc_B.read_u16() for _ in range(30)]) / 30
     
     v_A = raw_A * VOLTAGE_FACTOR
     v_B = raw_B * VOLTAGE_FACTOR
